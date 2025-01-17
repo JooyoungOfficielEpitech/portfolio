37597811893fddfe9034c4f7556f2c1f208234f9
KJY <jooyoung.kim@looxidlabs.com>
2025-01-17 18:22:53 +0900

    fix : docker compose 배포용

--- test.py
+++
@@ -1,8 +0,0 @@
-from redis import Redis
-
-try:
-    redis_client = Redis(host="localhost", port=6379)
-    redis_client.ping()
-    print("Connected to Redis")
-except Exception as e:
-    print("Redis connection error:", e)
